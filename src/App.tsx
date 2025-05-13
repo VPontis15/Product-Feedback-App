cea28f9ab619b5b02e71d6a1417f422d6fd2dcdf
Vassilis Pontis <vpontis15@gmail.com>
2025-05-13 23:59:40 +0300

    Refactor color variables in GlobalStyle and fix syntax errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ const GlobalStyle = createGlobalStyle`
 :root {
 --color-purple: #AD1FEA;
 --color-blue:#4661E6;
---color-black:rgb(55, 63, 104);
+--color-black:#373f68;
 --color-white: #ffffff;
 --color-light-gray:#F2F4FF;
 --color-gray:#F7f8FD;
 --color-dark-blue:#3A4374;
+--color-red:#D73737;
 --color-dark-gray:#647196;
 --color-orange:#F49F85;
 --color-cyan:#62BCFA;
+
+--color-purple-hover:#C75AF6
+--color-blue-hover:#7C91F9
+--color-black-hover:#656EA3;
+--color-white-hover:#F7F8FD;
+--color-red-hover:#E98888;
 
 --ff-primary: 'Jost', sans-serif;
 
@@ -25,6 +32,10 @@ const GlobalStyle = createGlobalStyle`
 --line-base: 1.5;
 --line-heading: 1.25;
 
+
+--btn-radius: 0.625rem;
+--btn-width: 9.875rem;
+--btn-height: 2.75rem;
 }
 
 * {
