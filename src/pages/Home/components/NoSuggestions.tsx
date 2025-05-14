95a13b8a1279d3695e83786968c42e7f1ec527fd
Vassilis Pontis <vpontis15@gmail.com>
2025-05-15 00:58:44 +0300

    Add NoSuggestions component to display a message when no feedback is available

---
+++ src/pages/Home/components/NoSuggestions.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import emptySuggestions from '../../../assets//suggestions/illustration-empty.svg';
+import Button from '../../../components/Button';
+const IlluStration = styled.img`
+  width: 100%;
+  max-width: 136px;
+  height: auto;
+  margin-block-end: 3.5rem;
+`;
+
+const EmptySuggestionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  color: var(--color-dark-gray);
+  background-color: var(--color-white);
+  border-radius: var(--btn-radius);
+  flex: 1;
+
+  h2 {
+    color: var(--color-dark-blue);
+    font-size: var(--fs-xxl);
+  }
+
+  p {
+    text-align: center;
+    max-width: 50ch;
+  }
+
+  p + button {
+    margin-block-start: 3rem;
+  }
+`;
+export default function NoSuggestions() {
+  return (
+    <EmptySuggestionContent>
+      <IlluStration src={emptySuggestions} alt="No suggestions" />
+      <h2>There is no feedback yet.</h2>
+      <p>
+        Got a suggestion? Found a bug that needs to be squashed? We love hearing
+        about new ideas to improve our app.
+      </p>
+      <Button variant="primary">+ Add Feedback</Button>
+    </EmptySuggestionContent>
+  );
+}
