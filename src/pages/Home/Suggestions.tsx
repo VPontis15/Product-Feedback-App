fe06fe8ead3ccb39e01e060e66ac4636e0b2cc86
Vassilis Pontis <vpontis15@gmail.com>
2025-05-16 00:20:33 +0300

    Refactor Suggestions component to display multiple Suggestion items conditionally

--- src/pages/Home/Suggestions.tsx
+++ src/pages/Home/Suggestions.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import NoSuggestions from './components/NoSuggestions';
 import SuggestionsHeader from './components/SuggestionsHeader';
+import Suggestion from './components/Suggestion';
 
 const Main = styled.main`
   display: flex;
@@ -8,12 +9,46 @@ const Main = styled.main`
   height: 100%;
   gap: 1.5rem;
 `;
-
+const SuggestionsWrapper = styled.div`
+  max-height: calc(100svh - 10rem);
+  overflow-y: auto;
+  display: flex;
+  scrollbar-color: var(--color-purple) var(--color-white);
+  scrollbar-width: 10px;
+  flex-direction: column;
+  overscroll-behavior-y: unset;
+  gap: 1.5rem;
+`;
 export default function Suggestions() {
+  const suggestion = {
+    id: '1',
+    title: 'Add a dark mode',
+    description: 'It would be great to have a dark mode option for the app.',
+    category: 'Feature',
+    likes: 120,
+    comments: 5,
+  };
+
+  const length = true;
+
   return (
     <Main>
       <SuggestionsHeader />
-      <NoSuggestions />
+      {length ? (
+        <SuggestionsWrapper>
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+          <Suggestion suggestion={suggestion} />
+        </SuggestionsWrapper>
+      ) : (
+        <NoSuggestions />
+      )}
     </Main>
   );
 }
