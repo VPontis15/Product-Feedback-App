44c275f3dcf654a25febe502477cb53a75855762
Vassilis Pontis <vpontis15@gmail.com>
2025-05-16 00:20:18 +0300

    Implement dynamic filter buttons with loading skeletons in Filters component

--- src/pages/Home/components/Filters.tsx
+++ src/pages/Home/components/Filters.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import Button from '../../../components/Button';
+import { useQuery } from '@tanstack/react-query';
+import supabase from '../../../api/supabase';
+import Skeleton from '../../../components/Skeleton';
+import ErrorMessage from '../../../components/ErrorMessage';
 
 const FiltersWrapper = styled.div`
   display: flex;
@@ -14,31 +18,58 @@ const FiltersWrapper = styled.div`
   width: 100%;
 
   @media (max-width: 768px) {
-    justify-content: center;
   }
 `;
 
+// Create a styled skeleton button
+const SkeletonButton = styled(Skeleton)`
+  width: 60px;
+  height: 25px;
+  border-radius: var(--btn-radius);
+`;
+
 export default function Filters() {
+  const {
+    data: filters,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['filters'],
+    queryFn: async () => {
+      try {
+        const { data: filters, error } = await supabase
+          .from('category')
+          .select('*');
+        if (error) {
+          throw new Error(error.message);
+        }
+        return filters;
+      } catch (error) {
+        console.error('Error fetching filters:', error);
+      }
+    },
+  });
+  if (error) {
+    return <ErrorMessage message={error.message || 'Something went wrong'} />;
+  }
+
   return (
     <FiltersWrapper>
       <Button isActive variant="filter" size="filter">
         All
       </Button>
-      <Button variant="filter" size="filter">
-        Ui
-      </Button>
-      <Button variant="filter" size="filter">
-        UX
-      </Button>
-      <Button variant="filter" size="filter">
-        Enhancement
-      </Button>
-      <Button variant="filter" size="filter">
-        Bug
-      </Button>
-      <Button variant="filter" size="filter">
-        Feature
-      </Button>
+      {isLoading
+        ? // Render skeleton filter buttons when loading
+          Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <SkeletonButton key={`skeleton-filter-${index}`} />
+            ))
+        : filters?.map((filter) => (
+            <Button key={filter.id} variant="filter" size="filter">
+              {filter.category}
+            </Button>
+          ))}
     </FiltersWrapper>
   );
 }
