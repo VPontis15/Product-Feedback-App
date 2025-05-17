bc9b009378361c62ca69d68a8347767a240fa91a
Vassilis Pontis <vpontis15@gmail.com>
2025-05-18 01:53:48 +0300

    Refactor GlobalStyle: update color-comment-divider and add color-skeleton; enhance text rendering properties

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,8 @@ const GlobalStyle = createGlobalStyle`
 --color-white-hover:#F7F8FD;
 --color-red-hover:#E98888;
 --color-comment: #8594F8;
---color-comment-divider:rgba(140, 146, 179, 0.13);
-
+--color-comment-divider:#8c92b3;
+--color-skeleton: var(--color-comment-divider);
 --ff-primary: 'Jost', sans-serif;
 
 --fs-xxs: 0.8125rem;
@@ -49,6 +49,11 @@ const GlobalStyle = createGlobalStyle`
 --grid-gap: 1.875rem;
 }
 
+*::selection {
+  background-color: var(--color-orange);
+  color: var(--color-white);}
+ 
+
 * {
   margin: 0;
   padding: 0;
@@ -58,6 +63,11 @@ body {
   font-size: var(--fs-base);
   line-height: var(--line-base);
   background-color: var(--color-light-gray);
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+text-wrap: balance;
+  -moz-osx-font-smoothing: grayscale;
+
 }
   img {
     max-width: 100%;
