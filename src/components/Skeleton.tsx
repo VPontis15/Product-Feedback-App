2f8e067ac35aad90b9f51a00f9391f4f6c5e9802
Vassilis Pontis <vpontis15@gmail.com>
2025-05-17 19:39:00 +0300

    Refactor Skeleton component to include width prop for better customization

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-const SkeletonDiv = styled.div`
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  width?: string;
+}
+
+const SkeletonDiv = styled.div<{ width?: string }>`
   animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
   border-radius: 0.375rem; /* rounded-md */
   background-color: var(--color-dark-blue);
+  width: ${(props) => props.width || 'auto'};
 
   @keyframes pulse {
     0%,
@@ -16,11 +21,8 @@ const SkeletonDiv = styled.div`
   }
 `;
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return <SkeletonDiv className={className} {...props} />;
+function Skeleton({ className, width, ...props }: SkeletonProps) {
+  return <SkeletonDiv className={className} width={width} {...props} />;
 }
 
 export default Skeleton;
