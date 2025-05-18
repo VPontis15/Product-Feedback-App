362e5a76d7154b8a28c658b6c99b9a40e1f1c2e7
Vassilis Pontis <vpontis15@gmail.com>
2025-05-18 17:39:23 +0300

    Enhance Header component: add mobile hamburger menu with toggle functionality

--- src/pages/Home/components/Header.tsx
+++ src/pages/Home/components/Header.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import backgroundHeader from '../../../assets/suggestions/desktop/background-header.png';
 import backgroundHeaderTablet from '../../../assets/suggestions/tablet/background-header.png';
 import backgroundHeaderMobile from '../../../assets/suggestions/mobile/background-header.png';
+import hamburgerIcon from '../../../assets/shared/mobile/icon-hamburger.svg';
+import closeMobileMenuIcon from '../../../assets/shared/mobile/icon-close.svg';
 
 const StyledHeader = styled.header`
   position: relative;
@@ -12,6 +14,18 @@ const StyledHeader = styled.header`
   border-radius: var(--btn-radius);
   overflow: hidden;
   padding-block: 3.875rem 1.5rem;
+
+  div:nth-child(2) {
+    display: flex;
+    justify-content: space-between;
+    items: center;
+  }
+
+  @media (max-width: 650px) {
+    padding-block: 1rem;
+    border-radius: 0;
+    overflow: auto;
+  }
 `;
 
 const StyledImage = styled.img`
@@ -25,6 +39,10 @@ const StyledImage = styled.img`
   object-position: center;
   overflow: hidden;
   border-radius: var(--btn-radius);
+
+  @media (max-width: 650px) {
+    border-radius: 0;
+  }
 `;
 
 const StyledText = styled.div`
@@ -37,15 +55,54 @@ const StyledText = styled.div`
     font-size: var(--fs-xl);
     line-height: var(--line-heading);
     font-weight: 700;
+
+    @media (max-width: 650px) {
+      font-size: var(--fs-base);
+    }
   }
   p {
     font-size: var(--fs-sm);
     line-height: var(--line-base);
     font-weight: 400;
+
+    @media (max-width: 650px) {
+      font-size: var(--fs-sm);
+    }
   }
 `;
 
-export default function Header() {
+const HamburgerButton = styled.button`
+      background: none;
+      border: none;
+      cursor: pointer;
+      display:none;
+      @media (max-width: 650px) {
+      display: block;
+    `;
+
+const SrOnly = styled.span`
+  border: 0;
+  clip: rect(0, 0, 0, 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+  white-space: nowrap;
+
+  /* For iOS devices */
+  user-select: none;
+`;
+
+interface HeaderProps {
+  isOpen: boolean;
+  handleIsOpen: (isOpen: boolean) => void;
+}
+export default function Header({ isOpen, handleIsOpen }: HeaderProps) {
+  const handleClick = () => {
+    handleIsOpen(!isOpen);
+  };
   return (
     <StyledHeader>
       <picture>
@@ -59,10 +116,21 @@ export default function Header() {
           alt="Header background"
         />
       </picture>
-      <StyledText>
-        <h1>Frontend Mentor</h1>
-        <p>Feedback Board</p>
-      </StyledText>
+      <div>
+        <StyledText>
+          <h1>Frontend Mentor</h1>
+          <p>Feedback Board</p>
+        </StyledText>
+        <HamburgerButton onClick={handleClick}>
+          <SrOnly>Menu</SrOnly>
+          <img
+            width={20}
+            height={17}
+            src={!isOpen ? hamburgerIcon : closeMobileMenuIcon}
+            alt=""
+          />
+        </HamburgerButton>
+      </div>
     </StyledHeader>
   );
 }
