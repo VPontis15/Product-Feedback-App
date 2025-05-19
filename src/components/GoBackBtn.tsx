d8274af2206f20949990c98b334ccb092e65d3b9
Vassilis Pontis <vpontis15@gmail.com>
2025-05-19 23:47:18 +0300

    Add GoBackBtn component for navigation with styled link

---
+++ src/components/GoBackBtn.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router';
+import arrowLeft from '../assets/shared/icon-arrow-left.svg';
+import styled from 'styled-components';
+
+const StyledGoBackBtn = styled(Link)`
+  display: inline-flex;
+  gap: 0.35rem;
+  align-items: center;
+  text-decoration: none;
+  text-transform: capitalize;
+  color: var(--color-dark-gray);
+  font-size: var(--fs-sm);
+`;
+
+export default function GoBackBtn() {
+  return (
+    <StyledGoBackBtn to="..">
+      <img src={arrowLeft} alt="" /> Go back
+    </StyledGoBackBtn>
+  );
+}
