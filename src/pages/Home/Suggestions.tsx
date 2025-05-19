fa055750d81827860741bb46ee20fdf1b7de3d3c
Vassilis Pontis <vpontis15@gmail.com>
2025-05-19 23:47:24 +0300

    Remove unused LoadingWrapper component from Suggestions page

--- src/pages/Home/Suggestions.tsx
+++ src/pages/Home/Suggestions.tsx
@@ -37,49 +37,6 @@ const SuggestionsWrapper = styled.div`
   }
 `;
 
-const LoadingWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: calc(100svh - 400px);
-  position: absolute;
-  top: 0;
-  width: 100%;
-  font-size: var(--fs-lg);
-  color: var(--color-dark-blue);
-  background-color: var(--color-white);
-  border-radius: var(--btn-radius);
-  padding: 2rem;
-
-  @media (max-width: 650px) {
-    top: 30px;
-    height: calc(100svh - 10rem);
-    padding: 0;
-    margin-block-start: 2rem;
-    border-radius: 0;
-  }
-
-  .loader {
-    width: 60px;
-    height: 50px;
-    --m: no-repeat linear-gradient(90deg, var(--color-cyan) 70%, transparent 0);
-    -webkit-mask: var(--m) calc(0 * 100% / 4) 100% / calc(100% / 5)
-        calc(1 * 100% / 5),
-      var(--m) calc(1 * 100% / 4) 100% / calc(100% / 5) calc(2 * 100% / 5),
-      var(--m) calc(2 * 100% / 4) 100% / calc(100% / 5) calc(3 * 100% / 5),
-      var(--m) calc(3 * 100% / 4) 100% / calc(100% / 5) calc(4 * 100% / 5),
-      var(--m) calc(4 * 100% / 4) 100% / calc(100% / 5) calc(5 * 100% / 5);
-    background: linear-gradient(var(--color-cyan) 0 0) left/0% 100% no-repeat
-      #ddd;
-    animation: l14 2s infinite steps(6);
-  }
-  @keyframes l14 {
-    100% {
-      background-size: 120% 100%;
-    }
-  }
-`;
-
 const ErrorWrapper = styled.div`
   display: flex;
   justify-content: center;
