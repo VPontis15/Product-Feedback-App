7121c080860155b1de454a9ae3018d473352b775
Vassilis Pontis <vpontis15@gmail.com>
2025-05-19 23:46:44 +0300

    Add Layout component for structured page layout with responsive design

---
+++ src/pages/NewSuggestion/components/Layout.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const RootLayout = styled.main`
+  display: grid;
+  grid-template-columns: 255px 1fr;
+  gap: var(--grid-gap);
+  max-width: min(1140px, 95%);
+  margin-inline: auto;
+  min-height: 100svh;
+  padding-block: 5.9375rem;
+
+  @media (max-width: 986px) {
+    grid-template-columns: 1fr;
+    padding-block: 3.875rem;
+    min-height: auto;
+    gap: 2.5rem;
+  }
+
+  @media (max-width: 650px) {
+    gap: 0;
+    padding-block: 0;
+    max-width: 100%;
+  }
+`;
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <RootLayout>{children}</RootLayout>;
+}
