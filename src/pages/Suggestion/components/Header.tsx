560876a80cc15554b1a74c84dac2572419065fd6
Vassilis Pontis <vpontis15@gmail.com>
2025-05-19 23:46:22 +0300

    Refactor Header component to replace Link with GoBackBtn for improved navigation

--- src/pages/Suggestion/components/Header.tsx
+++ src/pages/Suggestion/components/Header.tsx
@@ -1,30 +1,15 @@
-import { Link } from 'react-router';
 import styled from 'styled-components';
 import Button from '../../../components/Button';
-import arrowLeft from '../../../assets/shared/icon-arrow-left.svg';
+import GoBackBtn from '../../../components/GoBackBtn';
 const SuggestionHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
-  a {
-    display: inline-flex;
-    gap: 0.35rem;
-
-    align-items: center;
-    text-decoration: none;
-    text-transform: capitalize;
-    color: var(--color-dark-gray);
-    font-size: var(--fs-sm);
-  }
 `;
 export default function Header() {
   return (
     <SuggestionHeader>
-      <Link to="..">
-        {' '}
-        <img src={arrowLeft} alt="" /> Go back
-      </Link>
+      <GoBackBtn />
       <Button variant="secondary" size="sm">
         Edit Feedback
       </Button>
