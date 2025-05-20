5fd44b1b9364358c291750199320d42ed84b2dba
Vassilis Pontis <vpontis15@gmail.com>
2025-05-20 21:30:44 +0300

    Add RoadmapItemColumn component for displaying roadmap items with data fetching

---
+++ src/pages/Roadmap/components/RoadmapItemColumn.tsx
@@ -0,0 +1,194 @@
+import styled from 'styled-components';
+import Button from '../../../components/Button';
+import UpVotesBtn from '../../../components/Upvotes';
+import CommentLogo from '../../../components/CommentLogo';
+import supabase from '../../../api/supabase';
+import { useQuery } from '@tanstack/react-query';
+
+const StyledRoadmapItem = styled.article<{ color: string }>`
+  width: 100%;
+  border-top: 6px solid ${({ color }) => color};
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  padding-block: 1.5rem;
+  padding-inline: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  padding-inline: 2rem;
+  padding-block: 2rem;
+  align-items: start;
+  background-color: var(--color-white);
+
+  h4 {
+    margin-block-end: 0.5rem;
+  }
+
+  h3 {
+    font-size: var(--fs-lg);
+  }
+
+  p {
+    color: var(--color-dark-gray);
+  }
+`;
+
+const RoadmapItemColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const RoadmapItemContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  align-items: start;
+`;
+
+const StyledRoadmapItemColumnHeader = styled.div`
+  h2 {
+    font-size: var(--fs-lg);
+  }
+
+  p {
+    color: var(--color-dark-gray);
+  }
+`;
+
+const FeedBackItemColumnWrapper = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export default function RoadmapItemColumn({
+  title,
+  description,
+  color,
+  quantity = 0,
+}: {
+  title: string;
+  description: string;
+  quantity?: number;
+  color: string;
+}) {
+  const { data } = useQuery({
+    queryKey: ['roadmapColumn', title],
+    queryFn: async () => {
+      try {
+        const { data, error } = await supabase
+          .from('feedback')
+          .select(
+            `*,
+            status!inner(update_status),
+            comment(count),
+            category!inner(category)`
+          )
+          .eq('status.update_status', title);
+        if (error) throw new Error(error.message);
+        return data;
+      } catch (error) {
+        console.error('Error fetching roadmap column data:', error);
+        return [];
+      }
+    },
+  });
+
+  return (
+    <RoadmapItemColumnWrapper>
+      <RoadmapItemColumnHeader
+        quantity={quantity}
+        title={title}
+        description={description}
+      />
+      <FeedBackItemColumnWrapper>
+        {data?.map((feedback) => (
+          <RoadmapItem
+            key={feedback.id}
+            color={color}
+            feedback={{
+              id: feedback.id,
+              title: feedback.title,
+              description:
+                feedback.description.length > 55
+                  ? feedback.description.slice(0, 55) + '...'
+                  : feedback.description,
+              status: feedback.status.update_status,
+              category: feedback.category.category,
+              upvotes: feedback.upvotes,
+              comments: feedback.comment.length,
+            }}
+          />
+        ))}
+      </FeedBackItemColumnWrapper>
+    </RoadmapItemColumnWrapper>
+  );
+}
+
+function RoadmapItemColumnHeader({
+  title,
+  description,
+  quantity,
+}: {
+  title: string;
+  description: string;
+  quantity?: number;
+}) {
+  return (
+    <StyledRoadmapItemColumnHeader>
+      <h2>
+        {title} <span>({quantity})</span>
+      </h2>
+      <p>{description}</p>
+    </StyledRoadmapItemColumnHeader>
+  );
+}
+
+interface RoadmapItemProps {
+  feedback: {
+    id: string;
+    title: string;
+    description: string;
+    status: string;
+    category: string;
+    upvotes: number;
+    comments: number;
+  };
+  color: string;
+}
+
+function RoadmapItem({ feedback, color }: RoadmapItemProps) {
+  const { id, title, description, status, category, upvotes, comments } =
+    feedback;
+  return (
+    <StyledRoadmapItem color={color}>
+      <h4>{status}</h4>
+      <RoadmapItemContent>
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <Button size="sm" variant="filter">
+          {category}
+        </Button>
+        <ButtonsWrapper>
+          <UpVotesBtn
+            layout="horizontal"
+            upvotes={upvotes}
+            onClick={() => {
+              console.log('Upvoted!');
+            }}
+          />
+          <CommentLogo amount={2} />
+        </ButtonsWrapper>
+      </RoadmapItemContent>
+    </StyledRoadmapItem>
+  );
+}
