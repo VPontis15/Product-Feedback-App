9082939e8ca32e6c58602503a25c2aeb1136c01c
Vassilis Pontis <vpontis15@gmail.com>
2025-05-22 18:13:08 +0300

    Refactor Suggestions component to implement sorting functionality and enhance data fetching logic

--- src/pages/Home/Suggestions.tsx
+++ src/pages/Home/Suggestions.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import NoSuggestions from './components/NoSuggestions';
-import SuggestionsHeader from './components/SuggestionsHeader';
+import SuggestionsHeader, {
+  SORT_OPTIONS,
+  type SortOptionValue,
+} from './components/SuggestionsHeader';
 import Suggestion from './components/Suggestion';
 import { useQuery } from '@tanstack/react-query';
 import supabase from '../../api/supabase';
 import ErrorMessage from '../../components/ErrorMessage';
 import Loader from '../../components/Loader';
+import { useState } from 'react';
 
 const Main = styled.main`
   display: flex;
@@ -51,42 +55,46 @@ const ErrorWrapper = styled.div`
 `;
 
 export default function Suggestions() {
+  const [sortBy, setSortBy] = useState<SortOptionValue>(SORT_OPTIONS[0].value);
+
+  // Use the sort details in your query
   const {
     data: suggestions,
     error,
     isLoading,
   } = useQuery({
-    queryKey: ['suggestions'],
+    queryKey: ['suggestions', sortBy],
     queryFn: async () => {
-      try {
-        // Get feedback with category, status, and count comments for each feedback
-        const { data: suggestions, error } = await supabase
-          .from('feedback')
-          .select(
-            `
-          *,
-          category!inner(category),
-          status!inner(update_status),
-          comment(count)
-        `
-          )
-          .eq('status.update_status', 'Suggestion');
+      const currentSortOption =
+        SORT_OPTIONS.find((option) => option.value === sortBy) ||
+        SORT_OPTIONS[0];
 
-        if (error) {
-          throw new Error(error.message);
-        }
+      const { data } = await supabase
+        .from('feedback_with_comment_count')
+        .select(
+          `
+      *,
+      category!inner(category),
+      status!inner(update_status)
+    `
+        )
+        .eq('status.update_status', 'Suggestion')
+        .order(currentSortOption.column, {
+          ascending: currentSortOption.order === 'asc',
+        });
 
-        return suggestions;
-      } catch (error) {
-        console.error('Error fetching suggestions:', error);
-        throw error; // Re-throw to show in UI
-      }
+      return data || [];
     },
   });
+  console.log('Suggestions:', suggestions);
   const count = suggestions?.length || 0;
   return (
     <Main>
-      <SuggestionsHeader count={count} />
+      <SuggestionsHeader
+        count={count}
+        sortBy={sortBy}
+        onSortChange={setSortBy}
+      />
       {isLoading ? (
         <Loader fullscreen />
       ) : error ? (
