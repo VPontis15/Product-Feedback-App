121fe996794d4d7acac43bc8262eb9306512e5bc
Vassilis Pontis <vpontis15@gmail.com>
2025-05-24 12:54:43 +0300

    Wrap Aside and Suggestions components with FiltersProvider for improved filter state management

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import Layout from '../../components/Layout';
+import FiltersProvider from '../../context/filtersContext';
 import Aside from './Aside';
 import Suggestions from './Suggestions';
 
 export default function Home() {
   return (
     <Layout>
-      <Aside />
-      <Suggestions />
+      <FiltersProvider>
+        <Aside />
+        <Suggestions />
+      </FiltersProvider>
     </Layout>
   );
 }
