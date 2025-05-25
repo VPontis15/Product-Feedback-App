e2fedb0d11ebc2a5d3d607efdc5912569d93a44e
Vassilis Pontis <vpontis15@gmail.com>
2025-05-25 21:14:13 +0300

    Refactor Header component to conditionally render edit and delete buttons based on user authentication

--- src/pages/Suggestion/components/Header.tsx
+++ src/pages/Suggestion/components/Header.tsx
@@ -1,18 +1,39 @@
 import styled from 'styled-components';
 import Button from '../../../components/Button';
 import GoBackBtn from '../../../components/GoBackBtn';
+import { useAuth } from '../../../hooks/useAuth';
 const SuggestionHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  .actions {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
 `;
-export default function Header({ slug }: { slug: string }) {
+export default function Header({
+  slug,
+  feedbackUserId,
+}: {
+  slug: string;
+  feedbackUserId?: string;
+}) {
+  const { user } = useAuth();
   return (
     <SuggestionHeader>
       <GoBackBtn />
-      <Button to={`/feedback/${slug}/edit`} variant="secondary" size="sm">
-        Edit Feedback
-      </Button>
+      {user?.id === feedbackUserId && (
+        <div className="actions">
+          <Button to={`/suggestion/${slug}/delete`} variant="error" size="sm">
+            Delete Feedback
+          </Button>
+          <Button to={`/feedback/${slug}/edit`} variant="secondary" size="sm">
+            Edit Feedback
+          </Button>
+        </div>
+      )}
     </SuggestionHeader>
   );
 }
