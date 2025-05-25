ebf12e345661b4fa8a9c9258483a49e2b866e42c
Vassilis Pontis <vpontis15@gmail.com>
2025-05-25 21:15:11 +0300

    Add user authentication to Home component and log current user

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import Layout from '../../components/Layout';
 import FiltersProvider from '../../context/filtersContext';
+import { useAuth } from '../../hooks/useAuth';
 import Aside from './Aside';
 import Suggestions from './Suggestions';
 
 export default function Home() {
+  const { user } = useAuth();
+  console.log('Current user:', user);
+
   return (
     <Layout>
       <FiltersProvider>
