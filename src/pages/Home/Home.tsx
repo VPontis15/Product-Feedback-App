738d6905278de83949c80d8a32595a2984dd965c
Vassilis Pontis <vpontis15@gmail.com>
2025-05-28 20:36:23 +0300

    Remove unused authentication hook and console log from Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,9 @@
 import Layout from '../../components/Layout';
 import FiltersProvider from '../../context/filtersContext';
-import { useAuth } from '../../hooks/useAuth';
 import Aside from './Aside';
 import Suggestions from './Suggestions';
 
 export default function Home() {
-  const { user } = useAuth();
-  console.log('Current user:', user);
-
   return (
     <Layout>
       <FiltersProvider>
